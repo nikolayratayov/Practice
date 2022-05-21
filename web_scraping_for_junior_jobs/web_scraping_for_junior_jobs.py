11cefa6d03eb37d5ad30da5f247786b066f9dc25
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-05-21 11:00:00 +0300

    web scraping for multiple pages

--- web_scraping_for_junior_jobs/web_scraping_for_junior_jobs.py
+++ web_scraping_for_junior_jobs/web_scraping_for_junior_jobs.py
@@ -2,10 +2,22 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-req_python = requests.get('https://dev.bg/company/jobs/python/')
-python = req_python.text
-python = python.lower()
+page = 1
+url = 'https://dev.bg/company/jobs/python'
+python = requests.get(url).text.lower()
+while True:
+    page += 1
+    url += '/page/' + str(page)
+
+    temp = requests.get(url).text.lower()
+    soup = BeautifulSoup(temp, 'html.parser')
+    if len(soup.find_all(string=re.compile('404'))) > 0:
+        break
+
+    python += requests.get(url).text.lower()
+    url = 'https://dev.bg/company/jobs/python'
 
 eee = BeautifulSoup(python, 'html.parser')
-print(eee.find_all(string=re.compile('junior')))
+python_list = eee.find_all(string=re.compile('junior'))
 
+print(f'Python junior jobs - {len(python_list)}')
