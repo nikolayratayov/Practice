d8ca04bca3136c1b2ee19ff2e2566b74ac3ae15a
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-05-22 16:22:22 +0300

    update

--- flask/market/market/forms.py
+++ flask/market/market/forms.py
@@ -22,13 +22,13 @@ class RegisterForm(FlaskForm):
     email_address = StringField(label='Email address:', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
-    submit = SubmitField(label='Създай акаунт тъпак')
+    submit = SubmitField(label='Create account')
 
 
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
-    submit = SubmitField(label='влез тъпак')
+    submit = SubmitField(label='Submit')
 
 
 class PurchaseItemForm(FlaskForm):
